44ec779f070e70c76836cc1538e1e44a4bcbda2b
JIBINJOBY <jobyjibin264@gmail.com>
2025-10-28 13:53:06 +0000

    Fix: Remove sensitive flag from outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -21,10 +21,8 @@ output "db_connection_string" {
 
 output "acr_admin_username" {
   value     = azurerm_container_registry.acr.admin_username
-  sensitive = true
 }
 
 output "acr_admin_password" {
   value     = azurerm_container_registry.acr.admin_password
-  sensitive = true
 }
